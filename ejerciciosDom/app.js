1a646879506f4598a2cd4bcdffe733fe26affcc6
AaronS0 <aaronfsanchezg@gmail.com>
2024-05-08 09:29:00 -0500

    fin ejercicio 2

--- ejerciciosDom/app.js
+++ ejerciciosDom/app.js
@@ -3,20 +3,20 @@ let estatura, masa, resultadoImc, estaturaM2, boton, pesos, dolar, resultadoDivi
 estatura = document.getElementById("estatura")
 masa = document.getElementById("masa")
 boton = document.getElementById("boton")
-
 pesos = document.getElementById("pesos")
 dolar = document.getElementById("dolar")
-
-if (dolar.value != 0) {
-    resultadoDivisa = dolar.value * 3892.52
-} else if (pesos.value != 0) {
-    resultadoDivisa = pesos.value * 0.0002567
-}
-
 
 //Eventos
 boton.addEventListener("click", function () {
     estaturaM2 = (estatura.value / 100) * (estatura.value / 100)
     resultadoImc = masa.value / estaturaM2
     document.getElementById("resultado").value = resultadoImc.toFixed(3)
+})
+document.getElementById("pesos").addEventListener("keydown", function myFunction() {
+    resultadoDivisa = pesos.value * 0.0002567
+    document.getElementById("dolar").value = resultadoDivisa.toFixed(3)
+})
+document.getElementById("dolar").addEventListener("keydown", function myFunction() {
+    resultadoDivisa = dolar.value * 3892.52
+    document.getElementById("pesos").value = resultadoDivisa.toFixed(3)
 })
